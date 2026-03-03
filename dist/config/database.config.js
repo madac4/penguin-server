14c835913575b938db7424335dec73bc6b85bd6a
whhroo <orbupetru12@gmail.com>
2026-03-03 19:08:06 +0200

    fix build

--- dist/config/database.config.js
+++ dist/config/database.config.js
@@ -2,9 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.databaseConfig = void 0;
 exports.databaseConfig = {
-    uri: process.env.MONGODB_URI ??
-        process.env.DATABASE_URL ??
-        'mongodb://localhost:27017/penguin_db',
+    uri: process.env.DATABASE_URL ?? 'mongodb://localhost:27017/penguin_db',
     options: {
         maxPoolSize: 10,
     },
--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,8 +1,5 @@
 export const databaseConfig = {
-  uri:
-    process.env.MONGODB_URI ??
-    process.env.DATABASE_URL ??
-    'mongodb://localhost:27017/penguin_db',
+  uri: process.env.DATABASE_URL ?? 'mongodb://localhost:27017/penguin_db',
   options: {
     maxPoolSize: 10,
   },
